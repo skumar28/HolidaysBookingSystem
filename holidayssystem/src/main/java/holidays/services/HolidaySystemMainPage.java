989603f2812e490269e8a53d7e2603015ff3c0cb
Sandeep Kumar <skumar28@buffalo.edu>
2017-12-08 21:57:48 -0500

    Test

--- holidayssystem/src/main/java/holidays/services/HolidaySystemMainPage.java
+++ holidayssystem/src/main/java/holidays/services/HolidaySystemMainPage.java
@@ -3,8 +3,8 @@ package holidays.services;
 public class HolidaySystemMainPage{
 	
 	public static void main(String args[]) {
-		HolidaySearchFrame hsf = new HolidaySearchFrame("skumar");
-		//new LoginScreen();
+		//HolidaySearchFrame hsf = new HolidaySearchFrame("skumar");
+		new LoginScreen();
 	}
 	
 }
